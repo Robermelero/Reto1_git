5a49fe8570aa119c5fc67af9732b58f24fe9aee9
Roberto Melero <roberto.meleros@gmail.com>
2023-05-04 11:55:41 +0200

    subida

---
+++ reti1.js
@@ -0,0 +1,12 @@
+
+
+
+const suma = (op1, op2) => op1+op2;
+
+const resta = (op1, op2) => op1-op2;
+
+const multiplicacion = (op1, op2) => op1*op2;
+
+const division = (op1, op2) => op1/op2;
+
+const cuadrado = (op1) => op1*op1;
